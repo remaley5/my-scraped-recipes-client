46fa41cbf66c22819ed8e3f5bec2587ef3b05f1e
maddenlockin <maddenlockin@gmail.com>
2023-05-01 17:41:22 -0700

    placeholder loader working on search form submit

--- src/Components/CreateRecipes.js
+++ src/Components/CreateRecipes.js
@@ -1,98 +1,107 @@
-import Ingredients from './RecipeSearch/Ingredients';
-import Steps from './RecipeSearch/Steps';
-import SearchForm from './RecipeSearch/SearchForm';
-import RecipeEditor from './RecipeEditor';
-import React, {useState, useEffect} from 'react';
-import '../Styles/search.css';
-import '../Styles/forms.css';
+import Ingredients from "./RecipeSearch/Ingredients";
+import Steps from "./RecipeSearch/Steps";
+import SearchForm from "./RecipeSearch/SearchForm";
+import RecipeEditor from "./RecipeEditor";
+import React, { useState, useEffect } from "react";
+import "../Styles/search.css";
+import "../Styles/forms.css";
 
 function CreateRecipes() {
     const [ingredients, setIngredients] = useState({});
     const [steps, setSteps] = useState({});
 
-    const [progress, setprogress] = useState('SEARCH');
+    const [progress, setprogress] = useState("SEARCH");
 
     // Save state in localstorage
     useEffect(() => {
-        var storedSteps = JSON.parse(window.localStorage.getItem('steps'));
-        var storedIngredients = JSON.parse(window.localStorage.getItem('ingredients'));
-        var storedProgress = window.localStorage.getItem('progress');
-        if(storedSteps !== null) {
+        var storedSteps = JSON.parse(window.localStorage.getItem("steps"));
+        var storedIngredients = JSON.parse(
+            window.localStorage.getItem("ingredients")
+        );
+        var storedProgress = window.localStorage.getItem("progress");
+        if (storedSteps !== null) {
             setSteps(storedSteps);
-        } 
-        if(storedIngredients !== null) {
+        }
+        if (storedIngredients !== null) {
             setIngredients(storedIngredients);
         }
-        if(storedProgress !== null) {
+        if (storedProgress !== null) {
             setprogress(storedProgress);
         }
     }, []);
 
-
     // Update Recipe State
     const updateSteps = (updatedSteps) => {
-        setSteps({...updatedSteps});
-        window.localStorage.setItem('steps', JSON.stringify(updatedSteps));
-    }
+        setSteps({ ...updatedSteps });
+        window.localStorage.setItem("steps", JSON.stringify(updatedSteps));
+    };
 
     const updateIngredients = (updatedIngredients) => {
-        console.log('ingredients updating');
-        setIngredients({...updatedIngredients});
-        console.log('updatedIngredients', updatedIngredients);
-        window.localStorage.setItem('ingredients', JSON.stringify(updatedIngredients));
-    }
+        console.log("ingredients updating");
+        setIngredients({ ...updatedIngredients });
+        console.log("updatedIngredients", updatedIngredients);
+        window.localStorage.setItem(
+            "ingredients",
+            JSON.stringify(updatedIngredients)
+        );
+    };
 
     // Track Form Progress
     const handleFormProgress = (event, progressState) => {
         event.preventDefault();
-        console.log('setting progress state');
+        console.log("setting progress state");
         setprogress(progressState);
-        window.localStorage.setItem('progress', progressState);
-    }
+        window.localStorage.setItem("progress", progressState);
+    };
 
-  return (
-    <div>
-        <h1 className="create-heading"> Create your Recipe </h1>
-        {progress === 'SEARCH' ? (
-            <div>
-                <ul className="progressbar">
-                    <li className="first current">Search Recipe</li>
-                    <li className="second"> Edit Ingredients</li>
-                    <li className="third">Edit Instructions</li>
-                </ul>
-                <div className="search-top">
-                    <h2 id="search-recipe-label">Search by URL</h2>
+    return (
+        <div>
+            <h1 className="create-heading"> Create your Recipe </h1>
+            {progress === "SEARCH" ? (
+                <div>
+                    <ul className="progressbar">
+                        <li className="first current">Search Recipe</li>
+                        <li className="second"> Edit Ingredients</li>
+                        <li className="third">Edit Instructions</li>
+                    </ul>
+                    <div className="search-top">
+                        <h2 id="search-recipe-label">Search by URL</h2>
 
-                    <SearchForm 
-                        updateSteps={updateSteps} 
+                        <SearchForm
+                            updateSteps={updateSteps}
+                            updateIngredients={updateIngredients}
+                        />
+                    </div>
+                    <div className="results-wrap">
+                        <h2>Recipe</h2>
+                        <Steps steps={steps} />
+
+                        <Ingredients ingredients={ingredients} />
+
+                        <button
+                            className="accept submit-button"
+                            onClick={(event) =>
+                                handleFormProgress(event, "EDIT_INGREDIENTS")
+                            }
+                        >
+                            Accept and Edit
+                        </button>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <RecipeEditor
+                        handleFormProgress={handleFormProgress}
+                        updateSteps={updateSteps}
                         updateIngredients={updateIngredients}
+                        ingredients={ingredients}
+                        steps={steps}
+                        progress={progress}
                     />
                 </div>
-                <div className="results-wrap">
-                    <h2>Recipe</h2>
-                    <Steps steps={steps}/>
-
-                    <Ingredients ingredients={ingredients}/>
-
-                    <button 
-                        className="accept submit-button"
-                        onClick={(event) => handleFormProgress(event, 'EDIT_INGREDIENTS')}>Accept and Edit</button>
-                </div>
-            </div> 
-        ) : 
-            <div>
-                <RecipeEditor 
-                    handleFormProgress={handleFormProgress}
-                    updateSteps={updateSteps} 
-                    updateIngredients={updateIngredients}
-                    ingredients={ingredients} 
-                    steps={steps}
-                    progress={progress}
-                />
-            </div>
-        }
-    </div>
-  );
+            )}
+        </div>
+    );
 }
 
 export default CreateRecipes;
--- src/Components/RecipeSearch/SearchForm.js
+++ src/Components/RecipeSearch/SearchForm.js
@@ -1,74 +1,90 @@
-import React, {useState} from 'react';
-import '../../Styles/forms.css';
+import React, { useState } from "react";
+import "../../Styles/forms.css";
+import { MagnifyingGlass } from "react-loader-spinner";
 
-function SearchForm({updateSteps, updateIngredients}) {
+function SearchForm({ updateSteps, updateIngredients }) {
+    const [loading, setLoading] = useState(false);
 
-    const [url, setUrl] = useState('');
-    const [error, setErrorMessage] = useState('');
+    const [url, setUrl] = useState("");
+    const [error, setErrorMessage] = useState("");
 
     const handleChange = (event) => {
         setUrl(event.target.value);
-    }
+    };
 
     const scrapeRecipe = () => {
-        
+        setLoading(true);
         const searchUrl = url;
-        fetch('http://localhost:9000/scrape/useurl', {
-            method: 'post', 
-            headers: { 'Content-Type': 'application/json'}, 
-            body: JSON.stringify({url: searchUrl})
+        fetch("http://localhost:9000/scrape/useurl", {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ url: searchUrl }),
         })
-        .then((response) => { 
-            if(response.status >= 400) {
-                setErrorMessage("Sorry, we couldn't find that URL!");
-                throw new Error("Server responded with an error");
-            }
-            return response.json()
-        })
-        .then((responseData) => {
-            const stepsArr = [...responseData.data.steps];
-            const stepsObj = {};
-            stepsArr.forEach(function(step, idx) {
-                stepsObj[idx+1] = step;
-            });
+            .then((response) => {
+                if (response.status >= 400) {
+                    setLoading(false);
+                    setErrorMessage("Sorry, we couldn't find that URL!");
+                    throw new Error("Server responded with an error");
+                }
+                return response.json();
+            })
+            .then((responseData) => {
+                setLoading(false);
+                const stepsArr = [...responseData.data.steps];
+                const stepsObj = {};
+                stepsArr.forEach(function (step, idx) {
+                    stepsObj[idx + 1] = step;
+                });
 
-            const ingredientsArray = [...responseData.data.ingredients];
-            const ingredientsObj = {};
-            ingredientsArray.forEach(function(ingredient, idx) {
-                ingredientsObj[idx+1] = ingredient;
+                const ingredientsArray = [...responseData.data.ingredients];
+                const ingredientsObj = {};
+                ingredientsArray.forEach(function (ingredient, idx) {
+                    ingredientsObj[idx + 1] = ingredient;
+                });
+                //updateRecipe(ingredientsObj, stepsObj);
+                updateSteps(stepsObj);
+                updateIngredients(ingredientsObj);
             });
-            //updateRecipe(ingredientsObj, stepsObj);
-            updateSteps(stepsObj);
-            updateIngredients(ingredientsObj);
-        });
-      };
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
         scrapeRecipe();
-    }
+    };
 
-  return (
-    <div>
-        <form className="search-form" onSubmit={handleSubmit}>
-            <div className="control-wrap">
-                <input 
-                    id="url-input" 
-                    name="url" 
-                    type="text" 
-                    autoComplete='true'
-                    aria-describedby='error-message'
-                    aria-labelledby='search-recipe-label'
-                    value={url}
-                    onChange={handleChange}></input>
-                <button>Submit Search</button>
-            </div>    
-            <div 
-                className="error" 
-                id="error-message">{error}</div>
-        </form>
-    </div>
-  );
+    return loading ? (
+        <MagnifyingGlass
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="MagnifyingGlass-loading"
+            wrapperStyle={{}}
+            wrapperClass="MagnifyingGlass-wrapper"
+            glassColor="#c0efff"
+            color="#e15b64"
+        />
+    ) : (
+        <div>
+            <form className="search-form" onSubmit={handleSubmit}>
+                <div className="control-wrap">
+                    <input
+                        id="url-input"
+                        name="url"
+                        type="text"
+                        autoComplete="true"
+                        aria-describedby="error-message"
+                        aria-labelledby="search-recipe-label"
+                        value={url}
+                        onChange={handleChange}
+                    ></input>
+                    <button>Submit Search</button>
+                </div>
+                <div className="error" id="error-message">
+                    {error}
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default SearchForm;
